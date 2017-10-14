a20f7b6bd20e925a714942b257542a8f2630ddef
Yusuke Matsumoto <ymatsumoto@4cast.co.jp>
2017-10-14 18:44:53 +0900

    Revert "del_flg = 1 のユーザの投稿は / で表示しない"
    This reverts commit 0f703ddcf3e5f27883f13228ae5886d4f1b40226.

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -397,7 +397,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `del_flg` != 1 ORDER BY `created_at` DESC")
+	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
 	if err != nil {
 		fmt.Println(err)
 		return
